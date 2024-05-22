bc353fb9c128c8143feb126289392ef70865ca21
Süleyman Ezdemir <ezfesoftware@gmail.com>
2024-05-22 12:21:32 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Mobil Hafta 12',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  //--------------
+  //--------------
+  //--------------
+  //-------------- Değişkenler bu kısımda tanımlanacak.
+  //--------------
+  //--------------
+  //--------------
+  //--------------
+  //--------------
+  //--------------
+  //--------------
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text("Mobil Hafta 12"),
+        ),
+        body:
+            //--------------
+            //--------------
+            //--------------Kodları aşağıya yazınız.
+            //--------------
+            
+        //--------------
+        //--------------Kodları yukarıya yazınız.
+        //--------------
+        //--------------
+        //--------------
+        //--------------
+        //--------------
+        //--------------
+        //--------------
+        //--------------
+        //--------------
+        //--------------
+        //--------------
+        //--------------
+
+        );
+  }
+}
